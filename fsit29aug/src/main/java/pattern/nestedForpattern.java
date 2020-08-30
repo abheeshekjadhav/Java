aac250e744a3f70460e5b5c18a810787118a4f22
abheeshekjadhav <abheeshek.jadhav@gmail.com>
2020-08-30 11:07:07 +0530

    forpattern

---
+++ fsit29aug/src/main/java/pattern/nestedForpattern.java
@@ -0,0 +1,24 @@
+package pattern;
+import java.util.Scanner;
+
+
+public class nestedForpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the No of Rows:");
+		int row = Integer.parseInt(sc.next());
+		
+		for(int i = 1 ; i <= row ; i++) {
+			for (int col=1 ; col <=i ; col++ ) {
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
